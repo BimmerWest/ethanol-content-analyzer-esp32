78d394fa6a51318f2e609ac9a3b6043e93bca613
André Luiz Pastore Dambros <andre.dambros@mindera.com>
2026-04-02 00:40:13 -0300

    fix: update BLE device name to match new branding

--- src/main.h
+++ src/main.h
@@ -49,7 +49,7 @@ extern volatile uint32_t lastValidReadingMs;
 extern bool sensorTimedOut;
 
 // BLE UUIDs
-#define BLE_DEVICE_NAME         "Ethanol Analyzer"
+#define BLE_DEVICE_NAME         "BimmerWest Flex Kit"
 #define BLE_SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define BLE_ETHANOL_UUID        "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define BLE_TEMPERATURE_UUID    "beb5483f-36e1-4688-b7f5-ea07361b26a8"
